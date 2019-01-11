1769fec0c4a2ed5f5e4054c577c7628554d2666a
Kaden Weber <kweber142@gmail.com>
2019-01-11 14:07:49 -0700

    Project added

---
+++ WheelchairRemote/app/src/main/java/weber/kaden/wheelchairremote/MainActivity.java
@@ -0,0 +1,64 @@
+package weber.kaden.wheelchairremote;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    final int REQUEST_ENABLE_BT = 0;
+    //add UUID for the device here.
+
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Use this check to determine whether BLE is supported on the device. Then
+// you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+// displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+//        //start scan
+//        BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
+//            @Override
+//            public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+//
+//            }
+//        };
+//        mBluetoothAdapter.startLeScan(scanCallback);
+//
+    }
+
+    public UUID convertFromInteger(int i) {
+        final long MSB = 0x0000000000001000L;
+        final long LSB = 0x800000805f9b34fbL;
+        long value = i & 0xFFFFFFFF;
+        return new UUID(MSB | (value << 32), LSB);
+    }
+}
