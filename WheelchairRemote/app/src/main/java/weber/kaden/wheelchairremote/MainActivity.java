13713b7883243873c4be4f1dbb336e63e4348843
Kaden Weber <kweber142@gmail.com>
2019-01-11 22:53:08 -0700

    Adding bluetooth

--- WheelchairRemote/app/src/main/java/weber/kaden/wheelchairremote/MainActivity.java
+++ WheelchairRemote/app/src/main/java/weber/kaden/wheelchairremote/MainActivity.java
@@ -2,6 +2,10 @@ package weber.kaden.wheelchairremote;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
@@ -12,13 +16,29 @@ import android.widget.Toast;
 
 import java.util.UUID;
 
+import static android.bluetooth.BluetoothAdapter.STATE_CONNECTED;
+
 public class MainActivity extends AppCompatActivity {
     final int REQUEST_ENABLE_BT = 0;
+    //Device info: SH-HC-08
+    final String BLE_DEVICE_ADDRESS = "4C:3F:D3:02:DE:14";
+    //working characteristic with two descriptors
+    final UUID WORKING_CHARACTERISTIC = convertFromInteger(0xFFE0);
+    //FFE0
+    //FFE1 - valuew 0x34
+    //0x2902
+    //0x2901
     //add UUID for the device here.
+    //device information: 0x180A
+    //generic access: 0x1800
+    final UUID POSSIBLE_SERVICE_ID = convertFromInteger(0x1801);
+    //generic attribute 0x1801
 
+    final UUID UNKNOWN_SERVICE_UUID = convertFromInteger(0x2902);
 
     private BluetoothAdapter mBluetoothAdapter;
-
+    private BluetoothDevice mConnectedDevice;
+    private BluetoothGatt gatt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,16 +64,81 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
-//        //start scan
-//        BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
-//            @Override
-//            public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
-//
-//            }
-//        };
-//        mBluetoothAdapter.startLeScan(scanCallback);
-//
+        //start scan
+        BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
+            @Override
+            public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+                if (bluetoothDevice.getAddress().equals(BLE_DEVICE_ADDRESS)){
+                    mConnectedDevice = bluetoothDevice;
+                }
+            }
+        };
+        mBluetoothAdapter.startLeScan(scanCallback);
+
+        BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+            @Override
+            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                super.onServicesDiscovered(gatt, status);
+                //BluetoothGattCharacteristic characteristic = gatt.getService()
+                //BluetoothGattDescriptor descriptor = characteristic
+            }
+
+            @Override
+            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                super.onConnectionStateChange(gatt, status, newState);
+                if(newState == STATE_CONNECTED){
+                    gatt.discoverServices();
+                }
+            }
+
+            @Override
+            public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                super.onCharacteristicRead(gatt, characteristic, status);
+            }
+
+            @Override
+            public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                super.onCharacteristicWrite(gatt, characteristic, status);
+            }
+
+            @Override
+            public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                super.onCharacteristicChanged(gatt, characteristic);
+            }
+
+            @Override
+            public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+                super.onDescriptorRead(gatt, descriptor, status);
+            }
+
+            @Override
+            public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+                super.onDescriptorWrite(gatt, descriptor, status);
+            }
+        };
+        gatt = mConnectedDevice.connectGatt(this, true, gattCallback);
+
+
     }
+
+
+    // TODO: make sure all three of these stop scans
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
 
     public UUID convertFromInteger(int i) {
         final long MSB = 0x0000000000001000L;
